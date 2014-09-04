ed760eebf3b0993dad3551ce71f04f534be494d0
Sahil Amoli <sahilamoli@gmail.com>
2014-09-03 21:41:06 -0700

    Initial Commit

---
+++ tipsy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tipsy
+//
+//  Created by Sahil Amoli on 9/3/14.
+//  Copyright (c) 2014 box. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tipsy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tipsy
+//
+//  Created by Sahil Amoli on 9/3/14.
+//  Copyright (c) 2014 box. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
