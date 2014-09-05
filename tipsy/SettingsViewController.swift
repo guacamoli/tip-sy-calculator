1bd3a3fd3835d36fad67f7ff1891d65434641028
Sahil Amoli <sahilamoli@gmail.com>
2014-09-04 23:15:27 -0700

    -Working Tip Calculator -Settings View to change default tip percentage

---
+++ tipsy/SettingsViewController.swift
@@ -0,0 +1,55 @@
+//
+//  SettingsViewController.swift
+//  tipsy
+//
+//  Created by Sahil Amoli on 9/4/14.
+//  Copyright (c) 2014 box. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    // Constants
+    let defaultTipIndexKey = "default_tip_index"
+
+    // UI selectors
+    @IBOutlet weak var defaultTipControl: UISegmentedControl!
+    
+    // Helpers
+    // @TODO: Since both ViewControllers use similar helpers, ideally these would live in one place instead of repeated
+    func saveDefaultTipIndex(indexValue: Int) -> () {
+        var defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setInteger(indexValue, forKey: defaultTipIndexKey)
+        defaults.synchronize()
+    }
+
+    func retrieveDefaultTipIndex() -> Int {
+        var defaults = NSUserDefaults.standardUserDefaults()
+        return defaults.integerForKey(defaultTipIndexKey)
+    }
+
+    // Lifecycle methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        var defaultTipIndex = retrieveDefaultTipIndex()
+        defaultTipControl.selectedSegmentIndex = defaultTipIndex
+    }
+
+    // Action handlers
+    @IBAction func onBack(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    @IBAction func onDefaultTipValueChanged(sender: AnyObject) {
+        var newDefaultTip = defaultTipControl.selectedSegmentIndex
+        saveDefaultTipIndex(newDefaultTip);
+    }
+}
---
+++ tipsy/ViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ViewController.swift
+//  tipsy
+//
+//  Created by Sahil Amoli on 9/3/14.
+//  Copyright (c) 2014 box. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    // Constants
+    let defaultTipIndexKey = "default_tip_index"
+    let tipPercentages = [0.18, 0.20, 0.25]
+
+    // Member Vars
+    var currentKnownDefaultTipIndex = Int()
+
+    // UI selectors
+    @IBOutlet weak var tipAmountControl: UISegmentedControl!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    // Helpers
+    func getReadableDollarAmountFromDouble(value: Double) -> String {
+        return String(format: "$%.2f", value)
+    }
+
+    func retrieveDefaultTipIndex() -> Int {
+        var defaults = NSUserDefaults.standardUserDefaults()
+        return defaults.integerForKey(defaultTipIndexKey)
+    }
+
+    func setDefaultTipPercentageState(value: Int) -> () {
+        tipAmountControl.selectedSegmentIndex = value;
+        currentKnownDefaultTipIndex = value
+    }
+
+    // Lifecycle methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var defaultTipPercentageIndex = retrieveDefaultTipIndex()
+
+        // Set correct state for the segmented control on initial load
+        setDefaultTipPercentageState(defaultTipPercentageIndex)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        var defaultTipPercentageIndex = retrieveDefaultTipIndex()
+        
+        // Only update the state if the user has changed the the default tip percentage (when coming back to the main view)
+        // This allows for better user experience since the user could be in the middle of a calculation, then decides to go to the settings page but ends up not making a change -- when they come back to the main view, their transaction should not be modified!
+        if currentKnownDefaultTipIndex != defaultTipPercentageIndex {
+            setDefaultTipPercentageState(defaultTipPercentageIndex)
+        }
+
+    }
+
+    // Action handlers
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        var tipPercentage = tipPercentages[tipAmountControl.selectedSegmentIndex]
+        var billAmount = NSString(string: billField.text).doubleValue
+
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+        
+        tipLabel.text = getReadableDollarAmountFromDouble(tip)
+        totalLabel.text = getReadableDollarAmountFromDouble(total)
+    }
+
+    @IBAction func handleTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+}
